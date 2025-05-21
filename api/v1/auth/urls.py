d20e98c61f8080eb382f7894207de96df091c772
kevin7070 <kevin.7070@gmail.com>
2025-05-20 23:37:54 -0400

    clean up

--- api/v1/auth/urls.py
+++ api/v1/auth/urls.py
@@ -1,23 +1,5 @@
 # urls.py
 from django.urls import include, path
 
-from .views import (
-    CustomTokenRefreshView,
-    CustomTokenView,
-    LogoutView,
-    MeView,
-    get_csrf_token,
-)
-
-urlpatterns = [
-    path("get-csrf-token/", get_csrf_token, name="get_csrf_token"),
-    path("token/", CustomTokenView.as_view(), name="custom_token_obtain"),
-    path(
-        "token/refresh/", CustomTokenRefreshView.as_view(), name="custom_token_refresh"
-    ),
-    path("me/", MeView.as_view(), name="me_view"),
-    path("logout/", LogoutView.as_view(), name="logout_view"),
-]
-
-# Add dj-rest-auth
-urlpatterns += [path("dj-rest-auth/", include("dj_rest_auth.urls"))]
+# Add dj-rest-auth JWTCookieAuth
+urlpatterns = [path("dj-rest-auth/", include("dj_rest_auth.urls"))]
