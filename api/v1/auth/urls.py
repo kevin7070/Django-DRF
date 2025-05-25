2f812378189ebfe50c16a0c44080b311f71bda3a
kevin7070 <kevin.7070@gmail.com>
2025-05-25 01:01:10 -0400

    auth endpoints for cookie & token based

--- api/v1/auth/urls.py
+++ api/v1/auth/urls.py
@@ -1,5 +1,10 @@
-# urls.py
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Add dj-rest-auth JWTCookieAuth
-urlpatterns = [path("dj-rest-auth/", include("dj_rest_auth.urls"))]
+urlpatterns = [
+    # login with cookie-based JWT
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    # login using Authorization header
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
