8a89f6293c52ba33db5910402632b15515d5667e
kevin7070 <kevin.7070@gmail.com>
2025-06-05 12:48:08 -0400

    better urls position for dj-rest-auth

--- api/v1/auth/urls.py
+++ api/v1/auth/urls.py
@@ -5,11 +5,11 @@ from api.v1.auth.views import MyUserDetailsView, set_csrf_token
 
 urlpatterns = [
     # login with cookie-based JWT
+    path("nuxt/csrf/", set_csrf_token, name="set_csrf_token"),
     path("nuxt/user/", MyUserDetailsView.as_view(), name="my_user_details"),
     path("nuxt/", include("dj_rest_auth.urls")),
     # * auth/nuxt/login/      > login with username/email + password
     # * auth/nuxt/logout/     > logout
-    path("nuxt/csrf/", set_csrf_token, name="set_csrf_token"),
     # login using Authorization header
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
