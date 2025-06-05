48242bd9980783c7e5a9ef7c06d38485d8fc4a80
kevin7070 <kevin.7070@gmail.com>
2025-06-05 00:01:30 -0400

    endpoint to get csrftoken

--- api/v1/auth/urls.py
+++ api/v1/auth/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from api.v1.auth.views import set_csrf_token
+
 urlpatterns = [
     # login with cookie-based JWT
+    path("nuxt/csrf/", set_csrf_token, name="set_csrf_token"),
     path("nuxt/", include("dj_rest_auth.urls")),
     # * auth/nuxt/login/      > login with username/email + password
     # * auth/nuxt/user/       > get current user info
