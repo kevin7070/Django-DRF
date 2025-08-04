f291bce5d74e399624cf7f8484eb9ef609ae0881
kevin7070 <kevin.7070@gmail.com>
2025-08-04 14:43:13 -0400

    clean up validations

--- api/v1/account/director_views.py
+++ api/v1/account/director_views.py
@@ -23,20 +23,12 @@ class DirectorCompanyRoleViewSet(viewsets.ModelViewSet):
         return CompanyRole.objects.filter(company=self.request.user.company)
 
     def perform_create(self, serializer):
-        user = self.request.user
-        if not user.company_role or not user.company_role.has_perm("user", "manage"):
-            raise PermissionDenied("Permission denied")
-        serializer.save(company=user.company)
+        serializer.save(company=self.request.user.company)
 
     def perform_update(self, serializer):
-        role = self.get_object()
-        if role.company != self.request.user.company:
-            raise PermissionDenied("Permission denied")
         serializer.save()
 
     def perform_destroy(self, instance):
-        if instance.company != self.request.user.company:
-            raise PermissionDenied("Permission denied")
         if instance.is_protected:
             raise PermissionDenied("Permission denied")
         instance.delete()
@@ -57,8 +49,4 @@ class DirectorUserViewSet(viewsets.ModelViewSet):
         return User.objects.filter(company=self.request.user.company)
 
     def perform_update(self, serializer):
-        target_user = self.get_object()
-        actor = self.request.user
-        if target_user.company != actor.company:
-            raise PermissionDenied("Permission denied")
         serializer.save()
